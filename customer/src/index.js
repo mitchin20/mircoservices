75a4e7c34494db5e185df23e336baf0dfddf7c44
gndesktop <mitchin20@gmail.com>
2024-08-28 16:42:10 -0400

    use cors in index.js

--- customer/src/index.js
+++ customer/src/index.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
