24daee4d95c611af86dcab0164de9386bc5f9a97
mitchin20 <163489879+mitchin20@users.noreply.github.com>
2024-08-28 16:55:06 -0400

    Reconfig port read for gateway (#1)

--- shopping/src/index.js
+++ shopping/src/index.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3003;
 
 app.use(cors());
 app.use(express.json());
